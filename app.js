6f7635da6836e03199956f45a4e918b7a1704116
Maxson Chan <roymaxsonchanli98@gmail.com>
2021-05-24 23:26:01 +0800

    Removed hostname parameter from app.listen

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('*',checkUser);
 app.set('views', path.join(__dirname,'views/view'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public')));
-app.listen(3000);
+app.listen();
 
 
 app.use('/Product', Product);
